4c573db000d8402eac8b52f13f72b76cb9d2d7cb
Georgiy Starkov <gstarkov658@gmail.com>
2018-03-22 19:51:14 +0300

    Удалит неиспользуемый плагин при продакшене

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -38,7 +37,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(['static']),
     new ExtractTextPlugin({
       filename: './css/style.bundle.css',
       allChunks: true,
