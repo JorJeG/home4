464c9fd33558b05182ae6a1eef8e36d4bd32d481
Georgiy Starkov <gstarkov658@gmail.com>
2018-03-23 02:29:40 +0300

    Добавит шаблонизатор

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -58,8 +58,8 @@ module.exports = {
         to: './img',
       },
       {
-        from: path.join(__dirname, '../src/index.html'),
-        to: './',
+        from: path.join(__dirname, '../src/views'),
+        to: './views',
       },
     ]),
   ],
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || '3000';
 
-app.use(express.static('static'));
+app.set('views', path.join(__dirname, 'static', 'views'));
+app.set('view engine', 'pug');
 
-app.get('./', (req, res) => {
-  res.sendFile('index.html');
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hello' });
 });
 
 app.listen(PORT);
