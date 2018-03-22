a00a3795de66fb8d0277646c8446a522ce3ec3a3
Georgiy Starkov <gstarkov658@gmail.com>
2018-03-22 04:15:59 +0300

    Добавит сборку js файлов в разных режимах

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,9 +3,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    css: './src/css/main.css',
-  },
+  entry: [
+    './src/js/index.js',
+    './src/css/main.css',
+  ],
   output: {
     filename: 'js/bundle.js',
     path: path.join(__dirname, '../static'),
@@ -23,6 +24,14 @@ module.exports = {
           },
           ],
         }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env'],
+        },
       },
     ],
   },
