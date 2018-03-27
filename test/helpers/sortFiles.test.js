92d5308ad62e5b62faba988998731838f89f818a
Georgiy Starkov <gstarkov658@gmail.com>
2018-03-28 00:16:09 +0300

    Добавит тест функции sortFiles

--- test/helpers/sortFiles.test.js
+++ test/helpers/sortFiles.test.js
@@ -1,1 +1,24 @@
-console.log('Done');
+const sortFiles = require('../../helpers/sortFiles');
+const { assert } = require('chai');
+
+describe('Тестирование функции сортировки', function() {
+  it('Должна возвращать массив', function() {
+    const arrayFiles = [
+      {type: 'tree', hash: '234cf42', name: 'src'},
+      {type: 'blob', hash: '54f5c53', name: 'app.json'},
+      {type: 'tree', hash: '22ad543', name: 'config'}
+    ];
+    const sorted = sortFiles(arrayFiles);
+    assert.isArray(sorted);
+  });
+
+  it('Должна сортировать массив', function() {
+    const arrayFiles = [
+      {type: 'blob', hash: '54f5c53', name: 'app.json'},
+      {type: 'tree', hash: '22ad543', name: 'config'},
+      {type: 'tree', hash: '234cf42', name: 'src'},
+    ];
+    const sorted = sortFiles(arrayFiles);
+    assert.equal(sorted[0].name, 'config');
+  });
+});
