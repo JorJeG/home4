f367612df716563d33f75921a4762d32b695a32d
Georgiy Starkov <gstarkov658@gmail.com>
2018-03-28 04:17:50 +0300

    Добавит тест для функции показа содержимого файла
    + Добавит обработку ошибки, когда вводим неправильную чексумму
    для файла в url
    + Добавит дефолтную ветку в основном роутере

--- test/helpers/sortFiles.test.js
+++ test/helpers/sortFiles.test.js
@@ -2,11 +2,14 @@ const sortFiles = require('../../helpers/sortFiles');
 const { assert } = require('chai');
 
 describe('Тестирование функции сортировки', function() {
+  // Так как после команды git ls-tree нам приходит уже отсортированный массив файлов, то
+  // задача этой функции отсортировать файлы и папки
+
   it('Должна возвращать массив', function() {
     const arrayFiles = [
-      {type: 'tree', hash: '234cf42', name: 'src'},
       {type: 'blob', hash: '54f5c53', name: 'app.json'},
-      {type: 'tree', hash: '22ad543', name: 'config'}
+      {type: 'tree', hash: '22ad543', name: 'config'},
+      {type: 'tree', hash: '234cf42', name: 'src'},
     ];
     const sorted = sortFiles(arrayFiles);
     assert.isArray(sorted);
@@ -19,6 +22,11 @@ describe('Тестирование функции сортировки', functio
       {type: 'tree', hash: '234cf42', name: 'src'},
     ];
     const sorted = sortFiles(arrayFiles);
-    assert.equal(sorted[0].name, 'config');
+
+    assert.deepEqual(sorted, [
+      {type: 'tree', hash: '22ad543', name: 'config'},
+      {type: 'tree', hash: '234cf42', name: 'src'},
+      {type: 'blob', hash: '54f5c53', name: 'app.json'},
+    ]);
   });
 });
