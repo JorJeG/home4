1b959bcd9049d78f50bdfbb2ede4b8bfbe7470f9
Georgiy Starkov <gstarkov658@gmail.com>
2018-04-01 17:56:53 +0300

    Объединит конфиги вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,69 @@
-const DEV = require('./config/webpack.config.dev.js');
-const PROD = require('./config/webpack.config.prod.js');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 
-if (TARGET === 'dev') {
-  module.exports = DEV;
-}
-
-if (TARGET === 'prod') {
-  module.exports = PROD;
-}
+module.exports = {
+  mode: TARGET === 'dev' ? 'development' : 'production',
+  entry: [
+    './src/js/index.js',
+    './src/css/main.css',
+  ],
+  output: {
+    filename: 'js/bundle.js',
+    path: path.join(__dirname, 'static'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              minimize: !(TARGET === 'dev'),
+              sourceMap: TARGET === 'dev',
+            },
+          },
+          ],
+        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['static']),
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, './src/fonts'),
+        to: './fonts',
+      },
+      {
+        from: path.join(__dirname, './src/favicon'),
+        to: './favicon',
+      },
+      {
+        from: path.join(__dirname, './src/img'),
+        to: './img',
+      },
+      {
+        from: path.join(__dirname, './src/views'),
+        to: './views',
+      },
+    ]),
+  ],
+  watch: TARGET === 'dev',
+};
