c860ba97048887c14e168ff6f92b901d960934ff
Georgiy Starkov <gstarkov658@gmail.com>
2018-04-01 17:56:10 +0300

    Добавит хост и порт

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,5 @@
 const PATH_TO_REPO = './wallet-app/';
+const PORT = process.env.PORT || '3000';
+const HOST = process.env.HOST || '0.0.0.0';
 
-module.exports = { PATH_TO_REPO };
+module.exports = { PATH_TO_REPO, PORT, HOST };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || '3000';
+const { PORT, HOST } = require('./config/constants');
 
 app.set('views', 'static/views');
 app.set('view engine', 'pug');
@@ -11,4 +11,4 @@ app.set('view engine', 'pug');
 app.use(express.static('static'));
 app.use(router);
 
-app.listen(PORT);
+app.listen(PORT, HOST);
